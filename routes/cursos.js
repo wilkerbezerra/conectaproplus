808061d6002b1cdf19061bd0a0795f16ccd68385
Wilker Silva <wilker.bezerra1000@gmail.com>
2026-05-30 18:24:36 -0300

    feat: adicionar novos cursos e atualizar rotas

--- routes/cursos.js
+++ routes/cursos.js
@@ -40,4 +40,101 @@ router.get('/cursos/espetinho/aula/:numero', (req, res) => {
 
 });
 
+
+router.get('/cursos/geladinho-gourmet/aula/:numero', (req, res) => {
+
+  const numero = req.params.numero;
+
+  const course = courses.find(
+    c => c.slug === 'geladinho-gourmet'
+  );
+
+  if (!course) {
+    return res.status(404).send('Curso não encontrado');
+  }
+
+  const lesson = course.lessons.find(
+    l => l.number === numero
+  );
+
+  if (!lesson) {
+    return res.status(404).send('Aula não encontrada');
+  }
+
+  res.render('layouts/course-lesson', {
+  courseName: course.name,
+  lessonNumber: lesson.number,
+  lessonTitle: lesson.title,
+  videoUrl: lesson.videoUrl,
+  stylesheet: course.stylesheet,
+  lessons: course.lessons
+});
+
+});
+
+
+router.get('/cursos/comunicacao-e-oratoria/aula/:numero', (req, res) => {
+
+  const numero = req.params.numero;
+
+  const course = courses.find(
+    c => c.slug === 'comunicacao-e-oratoria'
+  );
+
+  if (!course) {
+    return res.status(404).send('Curso não encontrado');
+  }
+
+  const lesson = course.lessons.find(
+    l => l.number === numero
+  );
+
+  if (!lesson) {
+    return res.status(404).send('Aula não encontrada');
+  }
+
+  res.render('layouts/course-lesson', {
+  courseName: course.name,
+  lessonNumber: lesson.number,
+  lessonTitle: lesson.title,
+  videoUrl: lesson.videoUrl,
+  stylesheet: course.stylesheet,
+  lessons: course.lessons
+});
+
+});
+
+router.get('/cursos/informatica/aula/:numero', (req, res) => {
+
+  const numero = req.params.numero;
+
+  const course = courses.find(
+    c => c.slug === 'informatica'
+  );
+
+  if (!course) {
+    return res.status(404).send('Curso não encontrado');
+  }
+
+  const lesson = course.lessons.find(
+    l => l.number === numero
+  );
+
+  if (!lesson) {
+    return res.status(404).send('Aula não encontrada');
+  }
+
+  res.render('layouts/course-lesson', {
+  courseName: course.name,
+  lessonNumber: lesson.number,
+  lessonTitle: lesson.title,
+  videoUrl: lesson.videoUrl,
+  stylesheet: course.stylesheet,
+  lessons: course.lessons
+});
+
+});
+
+
+
 module.exports = router;
